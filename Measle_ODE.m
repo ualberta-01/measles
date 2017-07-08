f7b7a3221e0607b6e366b962dac2a9ebb6744d10
Hien Le <hienle@Hiens-MacBook-Air.local>
2017-07-08 16:07:54 -0600

    stochastic endemic equilibirum

--- Measle_ODE.m
+++ Measle_ODE.m
@@ -5,7 +5,7 @@ function Measle_ODE(N,S0,I0,V0,p,beta,mu,sigma,c)
 
 f = @(t,x) [p*mu*N - beta*x(1)*x(2)/N - mu*x(1); beta*x(1)*x(2)/N + sigma*beta*x(3)*x(2)/N - mu*x(2) - c*x(2);-sigma*beta*x(3)*x(2)/N - mu*x(3) + (1-p)*mu*N];
 
-[t, xa]= ode45(f, [0,50], [S0 I0 V0]);
+[t, xa]= ode45(f, [0,100], [S0 I0 V0]);
 
 plot(t,xa(:,2));
 
--- Measle_script.m
+++ Measle_script.m
@@ -15,8 +15,9 @@ clf;
 
 hold on
 Measle_ODE(N,S0,I0,V0,p,beta,mu,sigma,c);
-%[S,I,V,t] = Measle(N,S0,I0,V0,t0,p,beta,mu,sigma,c,n);
-%plot(t,I)
+[S,I,V,t] = Measle(N,S0,I0,V0,t0,p,beta,mu,sigma,c,n);
+plot(t,I)
+plot(t,V)
 %[S,I,V,t] = Measle(N,S0,I0,V0,t0,p,beta,mu,sigma,c,n);
 %plot(t,I)
 %[S,I,V,t] = Measle(N,S0,I0,V0,t0,p,beta,mu,sigma,c,n);
@@ -27,3 +28,59 @@ Measle_ODE(N,S0,I0,V0,p,beta,mu,sigma,c);
 %plot(t,I)
 hold off
 
+%endemic equilibrium
+N = 1000;
+S0 = 30;
+I0 = 20;
+V0 = 950;
+t0 = 0;
+p = 0.95;
+beta = 3;
+mu = 0.01;
+sigma = 0.2;
+c = 0.1;
+n = 10000;
+
+clf;
+
+hold on
+Measle_ODE(N,S0,I0,V0,p,beta,mu,sigma,c);
+[S,I,V,t] = Measle(N,S0,I0,V0,t0,p,beta,mu,sigma,c,n);
+plot(t,I)
+% [S,I,V,t] = Measle(N,S0,I0,V0,t0,p,beta,mu,sigma,c,n);
+% plot(t,I)
+% [S,I,V,t] = Measle(N,S0,I0,V0,t0,p,beta,mu,sigma,c,n);
+% plot(t,I)
+% [S,I,V,t] = Measle(N,S0,I0,V0,t0,p,beta,mu,sigma,c,n);
+% plot(t,I)
+% [S,I,V,t] = Measle(N,S0,I0,V0,t0,p,beta,mu,sigma,c,n);
+% plot(t,I)
+hold off
+
+N = 1000;
+S0 = 30;
+I0 = 20;
+V0 = 950;
+t0 = 0;
+p = 0.95;
+beta = 3;
+mu = 0.01;
+sigma = 0.1;
+c = 0.1;
+n = 10000;
+
+clf;
+
+hold on
+Measle_ODE(N,S0,I0,V0,p,beta,mu,sigma,c);
+[S,I,V,t] = Measle(N,S0,I0,V0,t0,p,beta,mu,sigma,c,n);
+plot(t,I)
+% [S,I,V,t] = Measle(N,S0,I0,V0,t0,p,beta,mu,sigma,c,n);
+% plot(t,I)
+% [S,I,V,t] = Measle(N,S0,I0,V0,t0,p,beta,mu,sigma,c,n);
+% plot(t,I)
+% [S,I,V,t] = Measle(N,S0,I0,V0,t0,p,beta,mu,sigma,c,n);
+% plot(t,I)
+% [S,I,V,t] = Measle(N,S0,I0,V0,t0,p,beta,mu,sigma,c,n);
+% plot(t,I)
+hold off
